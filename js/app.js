cc3f3e85e017a196e81358de19ccc8294721bde1
Geovani Reyes <javi23921@gmail.com>
2021-09-13 21:36:08 -0500

    add folders

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    toggle(".menu__toggle", ".menu__list", ".list__link");
+});
+
+function toggle(btn, nav__list, nav__link){
+    d.addEventListener("click", (e) => {
+        if(e.target.matches(btn) || e.target.matches(`${btn} *`)){
+            d.querySelector(nav__list).classList.toggle("active");
+            d.querySelector(btn).classList.toggle("active");
+        }
+        if(e.target.matches(nav__link)){
+            d.querySelector(nav__list).classList.remove("active");
+            d.querySelector(btn).classList.remove("active");
+        }
+        
+    })
+}
