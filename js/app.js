0be97b74c2d106bbb63d9eeaf4d5c29841819e65
Geovani Reyes <javi23921@gmail.com>
2021-09-14 20:47:31 -0500

    responsive

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ d.addEventListener("DOMContentLoaded", (e) => {
     toggle(".menu__toggle", ".menu__list", ".list__link");
 });
 
-function toggle(btn, nav__list, nav__link){
+function toggle(btn, nav__list, nav__link) {
     d.addEventListener("click", (e) => {
-        if(e.target.matches(btn) || e.target.matches(`${btn} *`)){
+        if (e.target.matches(btn) || e.target.matches(`${btn} *`)) {
             d.querySelector(nav__list).classList.toggle("active");
             d.querySelector(btn).classList.toggle("active");
         }
-        if(e.target.matches(nav__link)){
+        if (e.target.matches(nav__link)) {
             d.querySelector(nav__list).classList.remove("active");
             d.querySelector(btn).classList.remove("active");
         }
-        
+
     })
 }
